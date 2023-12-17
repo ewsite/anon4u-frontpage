3a17402ae1e5dce576973539421fad1d905cefd7
ewsite <rodel.rizalde09@gmail.com>
2023-12-17 10:19:35 +0800

    push this initial pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Button } from '$lib/components';
+
+</script>
+
+<svelte:head>
+	<title>anon4u: an anonymous dm.</title>
+</svelte:head>
+
+
+<div class="flex items-center justify-center h-full">
+	<div class="space-y-4">
+		<h1>anon4u</h1>
+		<h3>Download now!</h3>
+		<p>Only available on Android Devices</p>
+		<Button type="link" color="primary" href="/download">Download Now!</Button>
+	</div>
+</div>
---
+++ src/routes/download/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Button from "$lib/components/Button.svelte";
+
+</script>
+<svelte:head>
+    <meta http-equiv="refresh" content="5; url=/" />
+    <title>Download - anon4u</title>
+</svelte:head>
+
+<div class="flex h-full items-center justify-center">
+    <div class="flex flex-col space-y-4">
+        <h3>Download is now starting in 5 seconds...</h3>
+        <p>If not downloading automatically, try clicking Download button below</p>
+        <Button color="primary" type="link">Download </Button>
+    </div>
+</div>
